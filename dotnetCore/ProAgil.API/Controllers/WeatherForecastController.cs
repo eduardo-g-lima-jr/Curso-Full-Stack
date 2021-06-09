00cfbbbd8ef966994f1b77d93d0764745f67aa6e
eduardo-g-lima-jr <49029478+eduardo-g-lima-jr@users.noreply.github.com>
2021-06-09 15:19:04 -0300

    Salvando projeto

---
+++ dotnetCore/ProAgil.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return new Evento[]{
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e .Net Core",
+                    Local = "Jundiaí",
+                    Lote = "1º Lote",
+                    QtdPessoas = "250",
+                    DataEvento = ""
+                }
+            }
+        }
+    }
+}
